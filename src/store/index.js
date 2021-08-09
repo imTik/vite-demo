a1cb7947cf515978a5354b17a2c80acf2fc01790
Tik <275151754@qq.com>
2021-08-09 23:10:02 +0800

    新增store，

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from 'vuex';
+
+let store = {
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  mutations: {
+    countAdd(state) {
+      state.count++;
+    },
+  },
+  actions: {
+    countHandle({ commit }) {
+      commit('countAdd');
+    },
+  },
+  getters: {
+    COUNT: state => state.count
+  }
+};
+
+export default createStore(store);
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,19 +1,36 @@
 <!--  -->
 <template>
-  <div>
-    index page
+  <div class="home">
+    <img src="../assets/logo.png" alt="" />
+    <p>index page</p>
+    <button @click="countAdd">add count</button>
+    <p>{{count}}</p>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'index' ,
-  data () {
-    return {}
-  },
-}
+<script setup>
+import {
+  ref,
+  isRef,
+  toRefs,
+  computed,
+  reactive,
+  readonly,
+  watchEffect,
+  watch,
+} from 'vue';
+import { useStore, mapGetters, mapState, mapMutations, mapActions } from 'vuex';
+
+const store = useStore();
+let count = computed(() => store.getters.COUNT);
+
+function countAdd() {
+  store.dispatch('countHandle');
+};
 </script>
 
-<style lang='less' scoped>
-
+<style lang="less" scoped>
+.home {
+  text-align: center;
+}
 </style>
